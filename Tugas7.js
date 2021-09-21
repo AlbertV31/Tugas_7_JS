dcf0eb49bed457fdca1dd28d4b8e51f5f523f4bf
Albert <albert.bagil@gmail.com>
2021-09-21 14:11:06 +0700

    Tugas 7 Javascript
    Tugas 7 Javascript untuk dikumpulkan ke Niomic

---
+++ Tugas7.js
@@ -0,0 +1,10 @@
+var tinggi = [180,144,130,150,170,132,142,155,172, 166]
+
+for (var i = 0; i < tinggi.length; i++) {
+  console.log(tinggi[i]);
+}
+console.log("");
+
+for (var x of tinggi) {
+  console.log(x);
+}
